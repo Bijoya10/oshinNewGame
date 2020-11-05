f9eaa278259343b8267a73b1903b0f91e39e9609
bijoya <bijoya10101@gmail.com>
2020-11-05 18:40:21 +0530

    errorResolve

--- game.js
+++ game.js
@@ -1,10 +1,19 @@
 class Game{
     constructor(){
-        
+        this.choice=0;
+        this.button1 = createButton("Select");
+        this.button2 = createButton("Select");
+        this.button3 = createButton("Select");
+        this.button4 = createButton("START");
     }
 
     start(){
+
         if (gameState === 0){
+        this.button1.show();
+        this.button2.show();
+        this.button3.show();
+        this.button4.show();
         form = new Form();
         form.display();    
     }
@@ -14,49 +23,63 @@ choosePlayer(){
     image(player1_img,100,50,300,400);
     image(player2_img,600,50,300,450);
     image(player3_img,1000,50,300,450);
-    var button1 = createButton("Select");
-    var button2 = createButton("Select");
-    var button3 = createButton("Select");
-    var button4 = createButton("START");
-    button1.position(250,500);
-    button2.position(700,500);
-    button3.position(1100,500);
-    button4.position(620,580);
-    button4.style('height', '40px');
-    button4.style('width', '200px');
-    button4.style('background', 'pink');
+   
+    
+    this.button1.position(250,500);
+    this.button2.position(700,500);
+    this.button3.position(1100,500);
+    this.button4.position(620,580);
+    this.button4.style('height', '40px');
+    this.button4.style('width', '200px');
+    this.button4.style('background', 'pink');
+    if(this.choice===1){
+        console.log("hey")
+        this.button1.style("backgroundColor","red");
+        
+    }
+    if(this.choice===2){
+        this.button2.style("backgroundColor","red");
+    }
+    if(this.choice===3){
+        this.button3.style("backgroundColor","red");
+    }
 
-    button1.mousePressed(function(){
-        button1.style('background','red');
-        player.addImage(player1_img);
-          
+
+    this.button1.mousePressed(()=>{
+        console.log("hello")
+        this.choice=1;  
     }) 
-
-    button2.mousePressed(function(){
-        button3.style('background','red');
-        player.addImage(player2_img);
-         
-    }) 
-
-    button3.mousePressed(function(){
-        button3.style('background','red');
-        player.addImage(player3_img);
-      
-    }) 
-
-    button4.mousePressed(function(){
-        gameState = 2;
-        button1.hide();
-        button2.hide();
-        button3.hide();
-        button4.hide();
+    this.button2.mousePressed(()=>{
+        console.log("hi")
+        this.choice=2;
     })
+    this.button3.mousePressed(()=>{
+        this.choice=3;
+    }) 
+    
+   
+    this.button4.mousePressed(()=>{
+        this.button1.hide();
+        this.button2.hide();
+        this.button3.hide();
+        this.button4.hide();
+        if(this.choice===1){
+            player.addImage(player1_img);
+        }
+        else if(this.choice===2){
+            player.addImage(player2_img);
+         
+        }
+        else if(this.choice===3){
+            player.addImage(player3_img);
+        }
+        gameState = 2;
+       
+    })
+    
 }
 
 play(){
-    button1.hide();
-    button2.hide();
-    button3.hide();
-    button4.hide();
+   drawSprites();
 }
 }
